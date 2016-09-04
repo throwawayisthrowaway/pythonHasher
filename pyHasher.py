5cd852bb57b8ef2d6bacfd78ec8b833c1953d96f
GNoe Juan <GNoe Juan>
2016-09-04 18:57:18 -0500

    Check if intial target is file or folder

--- pyHasher.py
+++ pyHasher.py
@@ -9,6 +9,7 @@ def test():
 target = 'OS.js'
 ##
 def hashall(folder):
+  if os.path.isdir(folder):
     for f in os.listdir(folder):
         if os.path.isfile(os.path.join(folder,f)):
             try:
@@ -24,6 +25,19 @@ def hashall(folder):
                 print("error opening file")
         if os.path.isdir(os.path.join(folder,f)):
             hashall(os.path.join(folder,f))
+  if os.path.isfile(folder):
+    try:
+      with open(folder) as i:
+        buf = i.read()
+        try:
+          with open('hashlog.txt','a') as l:
+            hasher = hashlib.md5()
+            hasher.update(buf)
+            l.write(folder + ': ' + hasher.hexdigest()+'\n')
+        except IOError as exc:
+          print("error writing log")
+    except IOError as exc:
+      print("error opening file")
 ##Execution
 hashall(target)
 ##Previous attempt
